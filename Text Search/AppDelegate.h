a41135626c14a7b3a7ab594a604e0adcd8fc646c
Wu Peter <peter.wu@fanzytv.com>
2016-03-02 22:17:53 -0600

    Initial Commit

---
+++ Text Search/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Text Search
+//
+//  Created by Wu Peter on 2016/3/2.
+//  Copyright © 2016年 Wu Peter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Text Search/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Text Search
+//
+//  Created by Wu Peter on 2016/3/2.
+//  Copyright © 2016年 Wu Peter. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
